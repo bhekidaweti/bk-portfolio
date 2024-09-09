55fa683ebf6bbb9e76476e44b62e1911e49dbe8d
bhekidaweti <99840968+bhekidaweti@users.noreply.github.com>
2024-09-09 12:05:59 +0200

    Add files via upload
    Added new styling and icons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import Header from './components/Header';
 import Home from './components/Home';
-import AboutMe from './components/AboutMe';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
@@ -15,7 +14,7 @@ function App() {
   const [theme, setTheme] = useState('dark');
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' :'dark' ));
   };
 
   return (
@@ -26,7 +25,7 @@ function App() {
             <img src="profile-pic.png" alt="Profile" className="profile-image" />
           </div>
           <button className="theme-toggle-button" onClick={toggleTheme}>
-            {theme === 'light' ? (
+            {theme === 'dark' ? (
               <FontAwesomeIcon icon={faMoon} size="lg" />
             ) : (
               <FontAwesomeIcon icon={faSun} size="lg" />
@@ -36,7 +35,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutMe />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
