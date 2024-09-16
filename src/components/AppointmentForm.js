a4a842bc5bc3da151b2a463aeb2541232232333c
bhekidaweti <99840968+bhekidaweti@users.noreply.github.com>
2024-09-16 12:34:33 +0200

    Add files via upload
    Added appointment system

---
+++ src/components/AppointmentForm.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+const AppointmentForm = () => {
+  return (
+    <div className="appointment-form-container">
+      <h2>Book an Appointment</h2>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdVXDACcfvKhvoup43XXOELJbnwd6s-j99fHUuLxNUTxLBsBQ/viewform?embedded=true" 
+      width="640" 
+      height="959"
+       frameborder="0" 
+       marginheight="0" 
+       marginwidth="0">Loading…
+       </iframe>
+
+    </div>
+  );
+};
+
+export default AppointmentForm;
