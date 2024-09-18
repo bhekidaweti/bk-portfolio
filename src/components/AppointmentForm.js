2a85fec80788603bf7704416f0cb1b8db7260fb4
bhekidaweti <99840968+bhekidaweti@users.noreply.github.com>
2024-09-18 12:44:10 +0200

    Update AppointmentForm.js
    Fixed iframe empty title issue

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -5,7 +5,7 @@ const AppointmentForm = () => {
   return (
     <div className="appointment-form-container">
       <h2>Book an Appointment</h2>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdVXDACcfvKhvoup43XXOELJbnwd6s-j99fHUuLxNUTxLBsBQ/viewform?embedded=true" 
+      <iframe title="bheki-contact" src="https://docs.google.com/forms/d/e/1FAIpQLSdVXDACcfvKhvoup43XXOELJbnwd6s-j99fHUuLxNUTxLBsBQ/viewform?embedded=true" 
       width="640" 
       height="959"
        frameborder="0" 
